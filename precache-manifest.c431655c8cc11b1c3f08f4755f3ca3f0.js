90d3deaee19e6fd5e4b8896386dbf00b802579fb
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-12-21 22:45:52 +0100

    Updates

---
+++ precache-manifest.c431655c8cc11b1c3f08f4755f3ca3f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ac36c275da5138ba50abbc56cf2340",
+    "url": "/grocery-bud/index.html"
+  },
+  {
+    "revision": "77956f4fba531a1d256a",
+    "url": "/grocery-bud/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "0083711d63aeb373685e",
+    "url": "/grocery-bud/static/js/2.9a6867a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-bud/static/js/2.9a6867a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77956f4fba531a1d256a",
+    "url": "/grocery-bud/static/js/main.0221f5e1.chunk.js"
+  },
+  {
+    "revision": "24104236d27962cef5d9",
+    "url": "/grocery-bud/static/js/runtime-main.74f22f2c.js"
+  }
+]);
